8e63241007c00ac82a9914dea5e2d8c56963782f
Saritha Chinthapalli <saritha@snovasys.com>
2024-11-11 12:28:42 +0530

    allowed origin

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ import TaskRouter from './Routes/Tasks.js';
 
 dotenv.config()
 const app=express()
-app.use(cors())
+app.use(cors({
+    origin:["https://ems-frontend-974g.vercel.app/"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 app.use(express.json())
 app.use('/api/auth',router)
 app.use('/api/user',datarouter)
